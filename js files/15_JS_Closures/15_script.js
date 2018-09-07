5edd5a39bb6582cac76a0e0696d8f570170915ba
govardhanaaleswara96 <govardhanaaleswara88@gmail.com>
2018-09-07 17:54:20 +0530

    object added

---
+++ js files/15_JS_Closures/15_script.js
@@ -0,0 +1,17 @@
+// Closures Concept of JavaScript
+var a = 10;
+function greetOuter() {
+  var b = 20;
+  var greetInner = function () {
+      var c = 30;
+      console.log(a);//10
+      console.log(b);//20
+      console.log(c);//30
+  };
+  return greetInner;
+}
+function engine(fName) {
+  fName();
+}
+var greetFn = greetOuter();
+engine(greetFn);
